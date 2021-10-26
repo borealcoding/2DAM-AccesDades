a9d5adae0d5cf3ccedcc8e8127a0c18aebfc9fd5
Eidursson <borealcode.edv@gmail.com>
2021-10-26 16:16:50 +0200

    dades introductories

---
+++ AE3/src/es/florida/accesdades/ae3/Biblioteca.java
@@ -0,0 +1,10 @@
+package es.florida.accesdades.ae3;
+
+public class Biblioteca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AE3/src/es/florida/accesdades/ae3/Llibre.java
@@ -0,0 +1,10 @@
+package es.florida.accesdades.ae3;
+
+public class Llibre {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
