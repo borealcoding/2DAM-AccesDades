daa5d40b11bd8362eedb09a77c558588246e7741
Eidursson <borealcode.edv@gmail.com>
2021-10-26 18:04:44 +0200

    primers passos ae3

--- AE3/src/es/florida/accesdades/ae3/Biblioteca.java
+++ AE3/src/es/florida/accesdades/ae3/Biblioteca.java
@@ -1,7 +1,52 @@
 package es.florida.accesdades.ae3;
 
 public class Biblioteca {
-
+	/*
+	 * Metode: crearLlibre(Llibre llibre)
+	 * Funcio: 
+	 */
+	public int crearLlibre(Llibre llibre) {
+		
+	}
+	/*
+	 * Metode: recuperarLlibre(int identificador)
+	 * Funcio: 
+	 */
+	public Llibre recuperarLlibre(int identificador) {
+		
+	}
+	/*
+	 * Metode: mostrarLlibre(Llibre llibre)
+	 * Funcio: 
+	 */
+	public void mostrarLlibre(Llibre llibre) {
+		
+	}
+	/*
+	 * Metode: borrarLlibre(int identificador)
+	 * Funcio: 
+	 */
+	public void borrarLlibre(int identificador) {
+		
+	}
+	/*
+	 * Metode: actualitzaLlibre(int identificador)
+	 * Funcio: 
+	 */
+	public void actualitzaLlibre(int identificador) {
+		
+	}
+	/*
+	 * Metode: ArrayList<Llibre> recuperarTots()
+	 * Funcio: 
+	 */
+	public ArrayList<Llibre> recuperarTots() {
+		
+	}
+	/*
+	 * Metode: crearLlibre 
+	 * Funcio: 
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
