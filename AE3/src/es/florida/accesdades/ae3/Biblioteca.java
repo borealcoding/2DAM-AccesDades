0311849ea3b08bf083b15410f508fff9cd87b75e
Eidursson <borealcode.edv@gmail.com>
2021-10-27 20:12:51 +0200

    primers passos AE3

--- AE3/src/es/florida/accesdades/ae3/Biblioteca.java
+++ AE3/src/es/florida/accesdades/ae3/Biblioteca.java
@@ -1,55 +1,116 @@
 package es.florida.accesdades.ae3;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 public class Biblioteca {
 	/*
 	 * Metode: crearLlibre(Llibre llibre)
-	 * Funcio: 
+	 * Funcio: crear un nou llibre com a XML a partir de les dades proporcionades per l'usuari, torna l'identificador del llibre
 	 */
 	public int crearLlibre(Llibre llibre) {
+		return 0;
 		
 	}
 	/*
 	 * Metode: recuperarLlibre(int identificador)
-	 * Funcio: 
+	 * Funcio: torna un objecte llibre a partir d'un identificador
 	 */
 	public Llibre recuperarLlibre(int identificador) {
-		
+		return null;
 	}
 	/*
 	 * Metode: mostrarLlibre(Llibre llibre)
-	 * Funcio: 
+	 * Funcio: mostra els atributs del llibre per pantalla
 	 */
 	public void mostrarLlibre(Llibre llibre) {
 		
 	}
 	/*
 	 * Metode: borrarLlibre(int identificador)
-	 * Funcio: 
+	 * Funcio: borra un objecte llibre a partir d'un identificador
 	 */
 	public void borrarLlibre(int identificador) {
 		
 	}
 	/*
 	 * Metode: actualitzaLlibre(int identificador)
-	 * Funcio: 
+	 * Funcio: actualitza (modifica) la informacio d'un objecte llibre a partir d'un identificador
 	 */
 	public void actualitzaLlibre(int identificador) {
 		
 	}
 	/*
 	 * Metode: ArrayList<Llibre> recuperarTots()
-	 * Funcio: 
+	 * Funcio: torna una llista amb tots els llibres de la biblioteca
 	 */
 	public ArrayList<Llibre> recuperarTots() {
-		
+		return null;
 	}
 	/*
-	 * Metode: crearLlibre 
-	 * Funcio: 
+	 * Metode: main 
+	 * Funcio: mostra un menu amb diverses accions disponibles, el usuari podra seleccionar la que desitje
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
-}
+		Scanner sc = new Scanner(System.in);
+		int opcio = 0;
+		try {
+			String strFitxerXML = "Llibres.xml";
+            DocumentBuilderFactory dBuilderF = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dBuilderF.newDocumentBuilder();
+            Document document = dBuilder.parse(new File(strFitxerXML));
+            
+            
+        } catch(IOException e) {
+            e.printStackTrace();
+        } catch(ParserConfigurationException parserEx) {
+        	parserEx.printStackTrace();
+        } catch(SAXException saxEx) {
+        	saxEx.printStackTrace();
+        }
+		
+		// MENU 
+		System.out.println("Selecciona una de les opcions disponibles d'aquest menu:"
+				+ "\n\t1. Mostrar tots els titols de la biblioteca"
+				+ "\n\t2. Mostrar informacio detallada d'un llibre"
+				+ "\n\t3. Crear nou llibre"
+				+ "\n\t4. Actualitzar llibre"
+				+ "\n\t5. Borrar llibre"
+				+ "\n\t6. Tanca la biblioteca");
+		System.out.print("> ");
+		opcio = sc.nextInt();
+		switch(opcio) {
+		case 1:
+			System.out.println("1");
+			break;
+		case 2:
+			System.out.println("2");
+			break;
+		case 3:
+			System.out.println("3");
+			break;
+		case 4:
+			System.out.println("4");
+			break;
+		case 5:
+			System.out.println("5");
+			break;
+		case 6:
+			System.out.println("6");
+			break;
+		default:
+			System.err.println("ERROR! La opcio sel·leccionada no existeix");
+			
+		}
+	} // end-main
+} // end-class
--- AE3/src/es/florida/accesdades/ae3/Llibre.java
+++ AE3/src/es/florida/accesdades/ae3/Llibre.java
@@ -2,9 +2,80 @@ package es.florida.accesdades.ae3;
 
 public class Llibre {
 	
+	// identificador, titol, autor, any publicacio, editorial, num pagines
+	private int identificador, any_publicacio, numPagines;
+	private String titol, autor, editorial;
 	
-	public static void main(String[] args) {
+	public Llibre(int identificador, int any_publicacio, int numPagines, String titol, String autor, String editorial) {
+		super();
+		this.identificador = identificador;
+		this.any_publicacio = any_publicacio;
+		this.numPagines = numPagines;
+		this.titol = titol;
+		this.autor = autor;
+		this.editorial = editorial;
+	}
+
+	// ---- SETTERS
+	public void setId(int identificador) {
+		this.identificador = identificador;
+	}
+	
+	public void setTitol(String titol) {
+		this.titol = titol;
+	}
+	
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	public void setAnyPublicacio(int any_publicacio) {
+		this.any_publicacio = any_publicacio;
+	}
+	
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+	
+	public void setNumPagines(int numPagines) {
+		this.numPagines = numPagines;
+	}
+	
+	// ---- GETTERS
+	public int getId() {
+		return this.identificador;
+	}
+	
+	public String getTitol() {
+		return this.titol;
+	}
+	
+	public String getAutor() {
+		return this.autor;
+	}
+	
+	public int getAnyPublicacio() {
+		return this.any_publicacio;
+	}
+	
+	public String getEditorial() {
+		return this.editorial;
+	}
+	
+	public int getNumPagines() {
+		return this.numPagines;
+	}
+
+	@Override
+	public String toString() {
+		return "Llibre [identificador=" + identificador + ", any_publicacio=" + any_publicacio + ", numPagines="
+				+ numPagines + ", titol=" + titol + ", autor=" + autor + ", editorial=" + editorial + "]";
+	}
+
+	
+	
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+	}*/
 }
