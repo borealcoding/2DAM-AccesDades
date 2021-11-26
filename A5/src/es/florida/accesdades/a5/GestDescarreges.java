90709fe9f0694b2c66c524b73163c36b3bcb456e
Eidursson <borealcode.edv@gmail.com>
2021-11-26 21:18:55 +0100

    estructura basica

--- A5/src/es/florida/accesdades/a5/GestDescarreges.java
+++ A5/src/es/florida/accesdades/a5/GestDescarreges.java
@@ -4,8 +4,7 @@ public class GestDescarreges {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 2; int b = 3;
-		System.out.println("Hello "+(a+b));
+		
 	}
 
 }
